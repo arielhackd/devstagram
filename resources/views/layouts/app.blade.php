0f7a7bb6ec0202f197e29b387095423990264f59
Ariel Lemus <arielhackd@gmail.com>
2025-10-14 16:03:45 -0600

    FINALIZADO FORMULARIO REGISTRO

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,15 +3,33 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link rel="stylesheet" href="{{ asset('css/app.css')}}">
+        <script src="{{ asset('js/app.js')}}" defer></script>
         <title>DevStagram - @yield('titulo')</title>
         @vite('resources/css/app.css')
     </head>
 
-    <body>
+    <body class="bg-gray-100">
+        <header class="p-5 border-b bg-white shadow">
+            <div class="container mx-auto flex justify-between items-center">
+                <h1 class="text-3xl font-black">
+                    Devstagram
+                </h1>
 
-        <h1 class="text-4xl font-extrabold">@yield('titulo')</h1>
-        <h2>prueba de cambios</h2>
-        
+                <nav class="flex gap-2 items-center">
+                    <a class="font-bold uppercase text-gray-600 text-sm" href="#">Login</a>
+                    <a class="font-bold uppercase text-gray-600 text-sm" href="/crear-cuenta">Crear cuenta</a>
+                </nav>
+            </div>
+        </header>
+
+        <main class="container mx-auto mt-10">
+            <h2 class="font-black text-center text-3xl mb-10">@yield('titulo')</h2>
+            @yield('contenido')
+        </main>
+
+        <footer class="text-center p-5 text-gray-500 font-bold uppercase mt-10">
+            DevStagram - Todos los derechos reservados {{ now()->year }}
+        </footer>
     </body>
 </html>
