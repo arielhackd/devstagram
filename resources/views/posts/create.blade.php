98028b9303795ad46e55997df227f2c6ff787b02
Ariel Lemus <arielhackd@gmail.com>
2025-10-16 01:20:18 -0600

    CLASE 90 CREANDO PUBLICACION

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('titulo')
+    Crea una nueva publicación
+@endsection
+
+@section('contenido')
+    <div class="md:flex md:items-center">
+        <div class="md:w-1/2 px-10">
+            Imagen aquí.
+        </div>
+
+        <div class="md:w-1/2 px-10">
+            Descripción aquí.
+        </div>
+    </div>
+@endsection
