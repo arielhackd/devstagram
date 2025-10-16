e150f109d1a4b326b793981e770c6ee50d2d2fb3
Ariel Lemus <arielhackd@gmail.com>
2025-10-16 01:04:34 -0600

    FINALIZADO PERFIL ESTATICO DASHBOARD

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -11,8 +12,10 @@ class PostController extends Controller
         $this->middleware('auth');
     }
     //CREAMOS LA FUNCION INDEX PARA MOSTRAR RUTA
-    public function index()
+    public function index(User $user)
     {
-        return view('dashboard');
+        return view('dashboard',[
+            'user'=>$user
+        ]);
     }
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -50,6 +50,11 @@ Inicia Sesión en DevStagram
                     @enderror
                 </div>
 
+                <div class="mb-5">
+                    <input type="checkbox" name="remember">
+                    <label for="remember" class="text-sm text-gray-500">Mantener mi sesión abierta</label>
+                </div>
+
                 <input type="submit" value="Iniciar Sesión" class="bg-sky-600 hover:bg-sky-700 transition-colors cursorpointer uppercase font-bold w-full p-3 text-white rounded-lg">
             </form>
         </div>
